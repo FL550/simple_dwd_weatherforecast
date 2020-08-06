0d34c00804a3b062d94e07a8f01fe5a7ee14469e
FL550 <max.fermor@gmail.com>
2020-08-06 12:52:05 +0200

    Fix Licence

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
