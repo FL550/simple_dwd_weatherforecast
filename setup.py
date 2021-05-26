530d343d31df2bcfef74e92047d0c4888deb4c90
FL550 <max.fermor@gmail.com>
2021-05-26 12:55:45 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simple_dwd_weatherforecast",
-    version="1.0.17",
+    version="1.1.0",
     author="Max Fermor",
     description="A simple tool to retrieve a weather forecast from DWD OpenData",
     long_description=long_description,
