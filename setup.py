ee9e58173d1bd30ac8606d7a63ada9b6267b8601
FL550 <36160004+FL550@users.noreply.github.com>
2022-05-09 21:18:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simple_dwd_weatherforecast",
-    version="1.1.4",
+    version="1.1.5",
     author="Max Fermor",
     description="A simple tool to retrieve a weather forecast from DWD OpenData",
     long_description=long_description,
