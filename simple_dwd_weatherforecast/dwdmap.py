7ada5dca77b6bab11c001d000e60b9346fe8dfd8
FL550 <glacial0124@posteo.com>
2025-04-09 13:31:31 +0000

    Better error message

--- simple_dwd_weatherforecast/dwdmap.py
+++ simple_dwd_weatherforecast/dwdmap.py
@@ -207,6 +207,8 @@ def get_map(
             image.putdata(new_image_data)
         image = draw_marker(image, ImageBoundaries(minx, maxx, miny, maxy), markers)
         return image
+    else:
+        raise ConnectionError(f"Error during image request from DWD servers: {url}")
 
 
 class ImageLoop:
@@ -331,7 +333,7 @@ class ImageLoop:
         url = f"https://maps.dwd.de/geoserver/dwd/wms?service=WMS&version=1.3.0&request=GetMap&layers={layers}&bbox={self._miny},{self._minx},{self._maxy},{self._maxx}&width={self._image_width}&height={self._image_height}&srs=EPSG:4326&styles=&format=image/png&TIME={date.strftime('%Y-%m-%dT%H:%M:00.0Z')}"
         request = requests.get(url, stream=True)
         if request.status_code != 200 or request.headers["content-type"] != "image/png":
-            raise ConnectionError("Error during image request from DWD servers")
+            raise ConnectionError(f"Error during image request from DWD servers: {url}")
         try:
             image = Image.open(BytesIO(request.content))
         except Exception as e:
