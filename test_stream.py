c934d7186a6effb1318ceb7b8e0a451c3155a8b3
fl0x53 <70536684+fl0x53@users.noreply.github.com>
2026-05-11 18:14:08 +0200

    Refactor file reading to use context manager
    File to be opened with context manager to ensure proper cleanup even if exceptions occur.

--- test_stream.py
+++ test_stream.py
@@ -8,9 +8,8 @@ timestamp = datetime.now(timezone.utc)
 
 dwd_weather = dwdforecast.Weather("L511")
 
-in_file = open("development/MOSMIX_S_2023091815_240.kml", "rb")
-data = in_file.read()
-in_file.close()
+with open("development/MOSMIX_S_2023091815_240.kml", "rb") as in_file:
+    data = in_file.read()
 dwd_weather.parse_kml(data)
 
 print(dwd_weather.get_forecast_data(dwdforecast.WeatherDataType.PRECIPITATION_PROBABILITY, timestamp))
